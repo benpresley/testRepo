c03862d5372c19a222c2d46c47275a3e5d11c8ba
benpresley <apresley128314@troy.edu>
2018-08-04 22:13:59 -0500

    updated demo_module.js

--- demo_module.js
+++ demo_module.js
@@ -3,9 +3,10 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 var dt = require('./modules');
+var uc = require('upper-case'); //tells line 10 to display text in all caps. see variable name on line 6 & 10
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("The date and time are currently: " + dt.myDateTime());
+    res.write(uc("The date and time are currently: " + dt.myDateTime()));
     res.end('');
 }).listen(port);
